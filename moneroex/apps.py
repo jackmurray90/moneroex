a37393e24649ea710fa2eef557d608fa375cdfa8
Jack Murray <jack@murray.software>
2023-09-20 17:13:39 +1000

    Initial commit, basic index view

---
+++ moneroex/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MoneroExConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "moneroex"
---
+++ moneroex/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+from urllib.request import urlopen
+from json import loads
+from decimal import Decimal
+
+
+class IndexView(View):
+    def get(self, request):
+        with urlopen("https://api.kraken.com/0/public/Ticker?pair=XXMRXXBT") as f:
+            data = loads(f.read().decode("utf-8"))
+        ask, bid = [Decimal(data["result"]["XXMRXXBT"][i][0]) for i in "ab"]
+        return render(request, "index.html", {"ask": ask*Decimal("1.01"), "bid": bid*Decimal("0.99")})
